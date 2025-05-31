69e9a328e8ace4c8e243a32b1da27759d6e00f4d
Kenner Azevedo <53868486+keny-azvd@users.noreply.github.com>
2025-05-30 21:19:48 -0300

    Add files via upload
    adding files to repo

---
+++ so-thepiratebay/hello/main.c
@@ -0,0 +1,23 @@
+/*
+ *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
+ *    SPDX-FileCopyrightText: 2025 keny-azvd <kenner-azevedo@hotmail.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
+ *  and includes modifications made by the following author(s):
+ *  keny-azvd <kenner-azevedo@hotmail.com>
+ */
+
+#include <stdio.h>
+
+int main(void)   
+{
+  printf ("Hello World");
+  return 0;
+}
+
+
+
+
+
