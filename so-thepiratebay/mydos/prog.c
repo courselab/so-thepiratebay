b37b6566a05c931e340bb290321b17b7ad8d2e30
Rodrigo Valeretto <rodrigoaugustovaleretto@gmail.com>
2025-06-30 20:01:27 -0300

    Solution for mydos exercise

---
+++ so-thepiratebay/mydos/prog.c
@@ -0,0 +1,17 @@
+/*
+ *    SPDX-FileCopyrightText: 2025 keny-azvd <kenner-azevedo@hotmail.com>
+ *   
+ *    SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
+ *  and includes modifications made by the following author(s):
+ *  keny-azvd <kenner-azevedo@hotmail.com>
+ */
+
+#include "tydos.h"
+
+int main()
+{
+  puts("Hello World\n");
+  return 0;
+}
