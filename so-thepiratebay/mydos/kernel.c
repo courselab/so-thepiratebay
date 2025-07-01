4e369e3e819e3be5ae6d2fbb3efc8d52833fc040
Rodrigo Valeretto <rodrigoaugustovaleretto@gmail.com>
2025-06-30 22:43:56 -0300

    Changing OS name

--- so-thepiratebay/mydos/kernel.c
+++ so-thepiratebay/mydos/kernel.c
@@ -1,7 +1,7 @@
 /*
  *    SPDX-FileCopyrightText: 2021 Monaco F. J. <monaco@usp.br>
  *    SPDX-FileCopyrightText: 2025 keny-azvd <kenner-azevedo@hotmail.com>
- *   
+ *
  *    SPDX-License-Identifier: GPL-3.0-or-later
  *
  *  This file is a derivative of SYSeg (https://gitlab.com/monaco/syseg)
@@ -9,18 +9,18 @@
  *  keny-azvd <kenner-azevedo@hotmail.com>
  */
 
-/* This source file implements the kernel entry function 'kmain' called
-   by the bootloader, and the command-line interpreter. Other kernel functions
-   were implemented separately in another source file for legibility. */
+ /* This source file implements the kernel entry function 'kmain' called
+    by the bootloader, and the command-line interpreter. Other kernel functions
+    were implemented separately in another source file for legibility. */
 
 #include "kernel.h" /* Essential kernel functions.  */
 #include "bios1.h"  /* For kwrite() etc.            */
 #include "bios2.h"  /* For kread() etc.             */
 #include "kaux.h"   /* Auxiliary kernel functions.  */
 
-/* Kernel's entry function. */
-//extern void read_sector(unsigned char c, unsigned char h, unsigned char s,
-//                        unsigned short segment, unsigned short offset);
+    /* Kernel's entry function. */
+    //extern void read_sector(unsigned char c, unsigned char h, unsigned char s,
+    //                        unsigned short segment, unsigned short offset);
 
 
 void kmain(void)
@@ -46,7 +46,7 @@ void shell()
 {
   int i;
   clear();
-  kwrite("TinyDOS 1.0\n");
+  kwrite("ThePirateDOS 1.0\n");
 
   while (go_on)
   {
@@ -87,12 +87,12 @@ void shell()
    Function prototypes are in kernel.h. */
 
 struct cmd_t cmds[] =
-    {
-        {"help", f_help}, /* Print a help message.       */
-        {"quit", f_quit}, /* Exit TyDOS.                 */
-        {"hello", f_hello}, /* Execute an example program. */
-        {"list", fs_list},
-        {0, 0}};
+{
+    {"help", f_help}, /* Print a help message.       */
+    {"quit", f_quit}, /* Exit TyDOS.                 */
+    {"hello", f_hello}, /* Execute an example program. */
+    {"list", fs_list},
+    {0, 0} };
 
 /* Build-in shell command: help. */
 
@@ -119,14 +119,14 @@ void f_hello()
 
 void kputc(char c)
 {
-  __asm__ __volatile__ (
+  __asm__ __volatile__(
     "mov $0x0E, %%ah\n\t"   // Função 0Eh - escrever caractere em modo teletipo
     "mov %0, %%al\n\t"      // Caractere a ser impresso
     "int $0x10\n\t"         // Chama BIOS para imprimir
     :
-    : "r"(c)
+  : "r"(c)
     : "ax"
-  );
+    );
 }
 
 
@@ -137,38 +137,38 @@ void kputc(char c)
 #define NAME_SIZE 20
 
 // Implementada em ASM
-extern void bios_read(void *buffer, unsigned short sector_lba, unsigned short count);
+extern void bios_read(void* buffer, unsigned short sector_lba, unsigned short count);
 
 
 
 void fs_list() {
-    kwrite("You are in flist section !\n\n");
-    unsigned char buffer[SECTOR_SIZE * 3];
-    bios_read(buffer, START_SECTOR, 3);
+  kwrite("You are in flist section !\n\n");
+  unsigned char buffer[SECTOR_SIZE * 3];
+  bios_read(buffer, START_SECTOR, 3);
 
-    for (int i = 0; i < (SECTOR_SIZE * 2) / ENTRY_SIZE; i++) {
-        unsigned char *entry = buffer + i * ENTRY_SIZE;
+  for (int i = 0; i < (SECTOR_SIZE * 2) / ENTRY_SIZE; i++) {
+    unsigned char* entry = buffer + i * ENTRY_SIZE;
 
-        char dbg[32];
-        dbg[0] = 'E'; dbg[1] = ':'; dbg[2] = ' ';
-        dbg[3] = "0123456789ABCDEF"[(entry[0] >> 4) & 0xF];
-        dbg[4] = "0123456789ABCDEF"[entry[0] & 0xF];
-        dbg[5] = '\n';
-        dbg[6] = '\0';
-        kwrite(dbg);
+    char dbg[32];
+    dbg[0] = 'E'; dbg[1] = ':'; dbg[2] = ' ';
+    dbg[3] = "0123456789ABCDEF"[(entry[0] >> 4) & 0xF];
+    dbg[4] = "0123456789ABCDEF"[entry[0] & 0xF];
+    dbg[5] = '\n';
+    dbg[6] = '\0';
+    kwrite(dbg);
 
-        if (entry[0] == 0x00)
-            break;
+    if (entry[0] == 0x00)
+      break;
 
-        char name[NAME_SIZE + 1];
-        for (int j = 0; j < NAME_SIZE; j++) {
-            name[j] = entry[j];
-        }
-        name[NAME_SIZE] = '\0';
-
-        kwrite("Arquivo: ");
-        kwrite(name);
-        kwrite("\n");
+    char name[NAME_SIZE + 1];
+    for (int j = 0; j < NAME_SIZE; j++) {
+      name[j] = entry[j];
     }
+    name[NAME_SIZE] = '\0';
+
+    kwrite("Arquivo: ");
+    kwrite(name);
+    kwrite("\n");
+  }
 }
 
